ce0d61e4faeea980fa93123d4b7af0dbf7837028
Kyle Brown <picokyle@gmail.com>
2016-03-24 03:10:06 -0600

    add readable length formatting

--- mp3-lengths.py
+++ mp3-lengths.py
@@ -47,26 +47,38 @@ def file_lengths_recursive(files):
 
     return lengths
 
-def print_summary(lengths):
+def print_summary(lengths, print_readable):
     for song, length in sorted(lengths.items()):
         if length is None:
             print_error('{}: not an MP3'.format(song))
         elif isinstance(length, dict):
-            print_summary(length)
+            print_summary(length, print_readable)
         else:
-            print_length(song, length)
+            print_length(song, length, print_readable)
 
-def print_length(song, length):
+def print_length(song, length, print_readable):
     hours   = length // 3600
     minutes = (length % 3600) // 60
     seconds = length % 60
-    if hours > 0:
-        minutes 
-        print('%02d:%02d:%02d\t%s' % (hours, minutes, seconds, song))
+    if print_readable:
+        days = hours // 24
+        hours %= 24
+        weeks = days // 7
+        days %= 7
+        print('{}: '.format(song), end='')
+        if weeks   > 0: print('{} weeks, '  .format(weeks),   end='')
+        if days    > 0: print('{} days, '   .format(days),    end='')
+        if hours   > 0: print('{} hours, '  .format(hours),   end='')
+        if minutes > 0: print('{} minutes, '.format(minutes), end='')
+        print('{} seconds'.format(seconds))
     else:
-        print('%02d:%02d\t%s' % (minutes, seconds, song))
+        if hours > 0:
+            minutes 
+            print('%02d:%02d:%02d\t%s' % (hours, minutes, seconds, song))
+        else:
+            print('%02d:%02d\t%s' % (minutes, seconds, song))
 
-def print_total(lengths):
+def print_total(lengths, print_readable):
     def sum_dict(lengths):
         total_seconds = 0
         for song, length in sorted(lengths.items()):
@@ -77,7 +89,7 @@ def print_total(lengths):
             else:
                 total_seconds += length
         return total_seconds
-    print_length('Total', sum_dict(lengths))
+    print_length('Total', sum_dict(lengths), print_readable)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Print lengths of MP3 files')
@@ -89,6 +101,9 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--total',
                         dest='get_total', action='store_true',
                         help='only print the total length for all files')
+    parser.add_argument('-H', '--readable',
+                        dest='readable', action='store_true',
+                        help='print times in human readable format')
     parser.set_defaults(recursive=False, get_total=False)
     args = parser.parse_args()
 
@@ -98,6 +113,6 @@ if __name__ == '__main__':
         lengths = file_lengths(args.files)
 
     if args.get_total:
-        print_total(lengths)
+        print_total(lengths, args.readable)
     else:
-        print_summary(lengths)
+        print_summary(lengths, args.readable)
